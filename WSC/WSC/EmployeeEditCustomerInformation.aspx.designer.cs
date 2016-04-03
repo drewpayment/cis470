9aae3142b1a59efdae5701b1e0661cf3b9d9eecb
kolbra <kolb.rebecca54@gmail.com>
2016-04-03 11:08:28 -0500

    commit

--- WSC/WSC/EmployeeEditCustomerInformation.aspx.designer.cs
+++ WSC/WSC/EmployeeEditCustomerInformation.aspx.designer.cs
@@ -13,12 +13,21 @@ namespace WSC {
     public partial class EmployeeEditCustomerInformation {
         
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView GridViewEmployeeEdit;
     }
 }
